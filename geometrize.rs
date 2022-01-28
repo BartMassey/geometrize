bd3e4b0cbbc151c24e249fb31fbe2dadccb2a7b5
Bart Massey <bart.massey@gmail.com>
2022-01-27 17:47:02 -0800

    reworked toward recursive case

--- geometrize.rs
+++ geometrize.rs
@@ -33,27 +33,63 @@ fn decontrast(img: &mut SubImage<&mut GrayBuffer>, mean: f64) {
     }
 }
 
-fn geometrize(img: &mut GrayBuffer) {
+struct Cut {
+    coord: u32,
+    means: [f64; 2],
+    score: f64,
+}
+
+impl std::cmp::PartialEq<Cut> for Cut {
+    fn eq(&self, other: &Cut) -> bool {
+        self.cmp(other) == std::cmp::Ordering::Equal
+    }
+}
+
+impl std::cmp::Eq for Cut {}
+
+impl std::cmp::PartialOrd for Cut {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        OrderedFloat(self.score).partial_cmp(&OrderedFloat(other.score))
+    }
+}
+
+impl std::cmp::Ord for Cut {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+fn best_vcut(img: &SubImage<&mut GrayBuffer>) -> Cut {
     let (width, height) = img.dimensions();
-    let (y, m_top, m_bottom, _) = (1..height - 1)
+    (1..height - 1)
         .map(|y| {
             let top = img.view(0, 0, width, y);
             let (m_top, v_top) = image_stats(&top);
             let bottom = img.view(0, y, width, height - y);
             let (m_bottom, v_bottom) = image_stats(&bottom);
             let score = v_top * y as f64 + v_bottom * (height - y) as f64;
-            (y, m_top, m_bottom, score)
+            Cut {
+                coord: y,
+                means: [m_top, m_bottom],
+                score,
+            }
         })
-        .min_by_key(|&(_, _, _, v)| OrderedFloat(v))
-        .unwrap();
-    eprintln!("at {y} {m_top}/{m_bottom}");
-    decontrast(&mut img.sub_image(0, 0, width, y), m_top);
-    decontrast(&mut img.sub_image(0, y, width, height - y), m_bottom);
+        .min()
+        .unwrap()
+}
+
+fn geometrize(img: &mut SubImage<&mut GrayBuffer>) {
+    let (width, height) = img.dimensions();
+    let Cut { coord: y, means: [m1, m2], .. } = best_vcut(img);
+    eprintln!("at {y} {m1}/{m2}");
+    decontrast(&mut img.sub_image(0, 0, width, y), m1);
+    decontrast(&mut img.sub_image(0, y, width, height - y), m2);
 }
 
 fn main() {
     let argv: Vec<String> = std::env::args().collect();
     let mut img = ImageReader::open(&argv[1]).unwrap().decode().unwrap().into_luma16();
-    geometrize(&mut img);
+    let (width, height) = img.dimensions();
+    geometrize(&mut img.sub_image(0, 0, width, height));
     img.save(&argv[2]).unwrap();
 }
